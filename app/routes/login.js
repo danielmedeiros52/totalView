bb08b24301f669021580e62df7e8b01eac269672
Daniel Medeiros <danielmedeiros@MacBook-Pro-de-Daniel.local>
2018-07-12 23:54:17 -0300

    login

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,19 +1,22 @@
 
 module.exports = function (app) {
-    const pool =require('../infra/connectionFactory')
+    const pool = require('../infra/connectionFactory')
 
     app.get('/', (req, res) => {
         res.render('index')
     })
-    
+
     app.post('/', (req, res) => {
         var user = req.body
+
         console.log(user)
-        
-        pool.query('select * from usuario where login=$1',[user.login],(err,res)=>{
-            console.log(err ? err.stack : res.rows)
+
+        pool.query('select * from usuario where login=$1', [user.login], (err, res) => {
+           console.log(res.rows[0].length())
+            if (user.senha == res.rows[0].senha) { console.log('Logado com sucesso') }
+            else { console.log('login ou senha invalido') }
         })
     })
-    
+
 
 }
