3107f78a667b7efb5ea26f127ed5b84a2bd1a40a
Daniel Medeiros <danielmedeiros@MacBook-Pro-de-Daniel.local>
2018-07-19 09:36:49 -0300

    dashboard #1

---
+++ app/routes/dashboard.js
@@ -0,0 +1,15 @@
+
+module.exports = function (app) {
+
+
+    app.get('/dashboard', (req, res) => {
+        res.render('dashboard')
+    })
+
+    app.post('/dashboard', (req, res) => {
+   
+        
+    })
+
+
+}
