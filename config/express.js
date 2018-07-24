b9081a8acefbcd9d0f01e52fb959b306eb8a9553
Daniel Medeiros <danielmedeiros@MacBook-Pro-de-Daniel.local>
2018-07-23 23:33:51 -0300

    404

--- config/express.js
+++ config/express.js
@@ -19,6 +19,8 @@ module.exports = function () {
     app.use((req, res, next) => {
        if(req.session.user){
            res.locals.user = req.session.user
+       }else{
+        res.locals.user =false  
        }
        
         next()
