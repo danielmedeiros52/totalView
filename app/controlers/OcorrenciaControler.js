38254c7d9e31f7c4e05b85acdc90693d8a0e8845
rodolfogomes <rodolfoaguiargomes@gmail.com>
2018-11-10 17:04:34 -0300

    ajuste ocorrenciaDAO

--- app/controlers/OcorrenciaControler.js
+++ app/controlers/OcorrenciaControler.js
@@ -1,7 +1,39 @@
 class OcorrenciaControler{
 //  implementar métodos do controlsdor.
+    Cadastrar(){
+        let html = req.body;
+        let data = [html.nome,html.descricao];
+        
+        createConection(app).inserir(html.descricao,(err,result)=>{
+            if (result.rowCount != 0) {
+                res.send('Ocorrência já existe !');
+            } else {
+                inserir(res,data, createConection(app));
+            }
 
-}
+        });
+    }
+
+    update(){
+
+        let html = req.body;
+        let data = [html.nome,html.descricao];
+        
+        createConection(app).atualizar(html.id,(err,result)=>{
+            if (result.rowCount == 0) {
+                res.send('Ocorrência não encontrada!');
+            } else {
+                atualizar(res,data, createConection(app));
+            }
+
+        });
+    }
+
+}// Fim do controlador.
+
+
+
+
 
 function inserir(res,data, dao) {
     dao.inserir(data, (err, resultado) => {
@@ -11,6 +43,20 @@ function inserir(res,data, dao) {
         res.send(resultado)
     })
 }
+
+function atualizar(res,data, dao) {
+    dao.atualizar(data, (err, filtro,resultado) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(resultado)
+    })
+}
+
+
+
+
+
 function createConection(app) {
     const pool = app.infra.connectionFactory()
     let UsuarioDAO = new app.infra.OcorrenciaDAO(pool)
--- app/infra/OcorrenciaDAO.js
+++ app/infra/OcorrenciaDAO.js
@@ -8,16 +8,18 @@ class OcorrenciaDAO {
         this._connection.query('INSERT INTO OCORRENCIA (nome,descricao) VALUES ($1,$2)', data, callback);
     }
 
-    atualizar(data,callback,campo,filtro){
-        this._connection.query(`UPDATE OCORRENCIA SET ${campo} = $1 WHERE  ${filtro}`);
+    atualizar(data,filtro,callback){
+        this._connection.query(`UPDATE OCORRENCIA 
+                                SET NOME = $1 
+                                DESCRICAO = $2 WHERE  ${filtro}`,data,callback);
     }
 
-    excluir(data,callback,filtro){
+    excluir(data,filtro,callback){
         this._connection.query(`DELETE FROM OCORRENCIA WHERE  ${filtro}`);
     }
 
-    carregar(data,callback,filtro){
-        this._connection.query(`SELECT * FROM OCORRENCIA  WHERE ${campo} = ${filtro}`);
+    carregar(data,filtro,callback){
+        this._connection.query(`SELECT * FROM OCORRENCIA  WHERE ID = ${filtro}`);
     }
    
 }
